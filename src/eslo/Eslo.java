24b75e44f439d04ad924eb210aaa89f26e6ccfda
Axmart <>
2020-09-14 18:52:40 +0200

    BufferedReader , parseInt es Random

--- src/eslo/Eslo.java
+++ src/eslo/Eslo.java
@@ -40,20 +40,95 @@ public class Eslo {
 //        
 //        console.close();
         
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String akarmi = br.readLine();
-        
-        int szam = Integer.parseInt(br.readLine());
-        
-        Random rnd = new Random();
-        
-        int veletlenSzam = rnd.nextInt(5);
-        
-        
-        System.out.println(veletlenSzam);
-        
-        System.out.println("Koszonom");
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        
+//        String akarmi = br.readLine();
+//        
+//        int szam = Integer.parseInt(br.readLine());
+//        
+//        Random rnd = new Random();
+//        
+//        int veletlenSzam = rnd.nextInt(5);
+//        
+//        
+//        System.out.println(veletlenSzam);
+//        
+//        System.out.println("Koszonom");
+            //Feladatok.feladat5();
+            //Feladatok.feladat6();
+            //Feladatok.feladat7();
+            //Feladatok.feladat8();
+            //Feladatok.feladat9();
+            //Feladatok.feladat10();
     }
     
 }
+
+
+        class Feladatok{
+            
+            static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            
+            static void feladat5() throws IOException{
+                System.out.println("Kerem a teglalap a odlalat :");
+                int aOldal = Integer.parseInt(br.readLine());
+                System.out.println("Kerem a teglalap b oldalat");
+                int bOldal = Integer.parseInt(br.readLine());
+                System.out.println("A teglalap kerulete : " + (aOldal+bOldal)*2);
+                System.out.println("A teglalap terulete : "+ aOldal*bOldal);
+                
+            }
+            
+            static void feladat6() throws IOException{
+                
+                System.out.println("Kerem a kor sugarat");
+                double sugar = Double.parseDouble(br.readLine());
+                System.out.println("A kor Sugara : " + 2*Math.PI*sugar);
+                System.out.println("A kor Terulete : " + Math.PI*Math.pow(sugar, 2));
+                
+                
+            }
+            
+            
+            static void feladat7() throws IOException {
+                
+                System.out.println("Kerem az egyik befogot :");
+                double a = Double.parseDouble(br.readLine());
+                double b = Double.parseDouble(br.readLine());
+                System.out.println("Kerem a masik befogot :");
+                System.out.println("Az atfogo = "+ Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2)));
+                
+            }
+            
+            static void feladat8() throws IOException{
+                System.out.println("Kerem az ut hosszat meterben: ");
+                double hossz = Double.parseDouble(br.readLine());
+                System.out.println("Kerem az eltelt idot percben :");
+                int ido = Integer.parseInt(br.readLine());
+                System.out.println("Az atlagsebesseg = " + (hossz/1000)/ido*60 + " km/h");
+                
+            }
+            
+            static void feladat9() throws IOException{
+                
+                System.out.println("Kerem az auto fogyasztasat 100 km-en");
+                double fogyasztas = Double.parseDouble(br.readLine());
+                System.out.println("Kerem a benzin literenkenti arat : ");
+                double benzinAr = Double.parseDouble(br.readLine());
+                System.out.println("Kerem a megtett ut hosszat Km-ben :");
+                double megtettUt = Double.parseDouble(br.readLine());
+                System.out.println("Az utikoltseg = " + fogyasztas/100*benzinAr*megtettUt);
+            }   
+            
+            static  void feladat10() throws IOException{
+                System.out.println("Kerem a magassagot cm-ben :");
+                double magassag = Double.parseDouble(br.readLine());
+                System.out.println("Kerem a sulyt kg-ban :");
+                double suly = Double.parseDouble(br.readLine());
+                System.out.println("A testomegindex : " + suly /Math.pow(magassag/100, 2));
+                
+                
+        }       
+            
+            
+        }
